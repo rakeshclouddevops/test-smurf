567e54ea9fcac8039b76871ecb0448188e5531ed
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-10-29 23:37:52 +0530

    feat: terraform apply cmd

---
+++ cmd/apply/apply.go
@@ -0,0 +1,20 @@
+package apply
+
+import (
+	"github.com/clouddrove/smurf/cmd"
+	"github.com/clouddrove/smurf/internal/terraform"
+	"github.com/spf13/cobra"
+)
+
+var applyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "Apply the changes required to reach the desired state of Terraform Infrastructure",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		return terraform.Apply()
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(applyCmd)
+}
