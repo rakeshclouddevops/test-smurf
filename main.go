f176d56ac92e16034c0c4278275da455401abe25
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-10-29 23:34:45 +0530

    feat: added terraform commands

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "smurf/cmd"
+import (
+	"github.com/clouddrove/smurf/cmd"
+	_ "github.com/clouddrove/smurf/cmd/apply"
+	_ "github.com/clouddrove/smurf/cmd/destroy"
+	_ "github.com/clouddrove/smurf/cmd/drift"
+	_ "github.com/clouddrove/smurf/cmd/init"
+	_ "github.com/clouddrove/smurf/cmd/output"
+	_ "github.com/clouddrove/smurf/cmd/plan"
+	_ "github.com/clouddrove/smurf/cmd/provision"
+)
 
 func main() {
 	cmd.Execute()
