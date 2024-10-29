1756c135e0fc0bf5b375f1417677ec0a26600134
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-10-29 23:38:31 +0530

    feat: terraform init cmd

---
+++ cmd/init/init.go
@@ -0,0 +1,20 @@
+package init
+
+import (
+	"github.com/clouddrove/smurf/cmd"
+	"github.com/clouddrove/smurf/internal/terraform"
+	"github.com/spf13/cobra"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "Initialize Terraform",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		return terraform.Init()
+	},
+}
+
+func init() {
+	cmd.RootCmd.AddCommand(initCmd)
+}
