aa7588546454434762935116a0e9718223f6ca41
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-10-31 01:15:19 +0530

    refactor: execution speed fix

--- cmd/terraform/provision.go
+++ cmd/terraform/provision.go
@@ -1,6 +1,8 @@
 package terraform
 
 import (
+	"sync"
+
 	"github.com/clouddrove/smurf/cmd"
 	"github.com/clouddrove/smurf/internal/terraform"
 	"github.com/spf13/cobra"
@@ -10,32 +12,45 @@ var provisionCmd = &cobra.Command{
 	Use:   "provision",
 	Short: "Its the combination of init, drift, plan, apply, output for Terraform",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		err := terraform.Init()
-
-		if err != nil {
+		var wg sync.WaitGroup
+		errChan := make(chan error, 5) // Buffer to store up to 5 errors
+		if err := terraform.Init(); err != nil {
 			return err
 		}
 
-		err = terraform.DetectDrift()
-
-		if err != nil {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := terraform.DetectDrift(); err != nil {
+				errChan <- err
+			}
+		}()
+		if err := terraform.Plan(); err != nil {
 			return err
 		}
 
-		err = terraform.Plan()
-
-		if err != nil {
+		if err := terraform.Apply(); err != nil {
 			return err
 		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := terraform.Output(); err != nil {
+				errChan <- err
+			}
+		}()
 
-		err = terraform.Apply()
+		wg.Wait()
 
-		if err != nil {
-			return err
+		close(errChan)
+
+		for err := range errChan {
+			if err != nil {
+				return err // Return the first error encountered
+			}
 		}
 
-		return terraform.Output()
+		return nil
 	},
 }
 
