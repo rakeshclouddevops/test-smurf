b1c617a5aae07852e9ca1bccfaec80d04a627314
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-06 01:58:38 +0530

    feat: install cmd for helm

---
+++ cmd/helm/install.go
@@ -0,0 +1,21 @@
+package helm
+
+import (
+	"github.com/clouddrove/smurf/internal/helm"
+	"github.com/spf13/cobra"
+)
+
+var installCmd = &cobra.Command{
+	Use:   "install [NAME] [CHART]",
+	Short: "Install a Helm chart into a Kubernetes cluster.",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		releaseName := args[0]
+		chartPath := args[1]
+		return helm.HelmInstall(releaseName, chartPath, "default") // Assuming 'default' namespace or make it a flag
+	},
+}
+
+func init() {
+	selmCmd.AddCommand(installCmd)
+}
