65126af72f6484dfd72e3a3e59121d5f867a5231
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-06 01:58:55 +0530

    feat: lint cmd for helm

---
+++ cmd/helm/lint.go
@@ -0,0 +1,20 @@
+package helm
+
+import (
+	"github.com/clouddrove/smurf/internal/helm"
+	"github.com/spf13/cobra"
+)
+
+var lintCmd = &cobra.Command{
+	Use:   "lint [CHART]",
+	Short: "Lint a Helm chart.",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		chartPath := args[0]
+		return helm.HelmLint(chartPath)
+	},
+}
+
+func init() {
+	selmCmd.AddCommand(lintCmd)
+}
