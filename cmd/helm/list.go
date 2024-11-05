90a40c8f638325905e14afd9dae32d7cf73463a3
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-06 01:59:18 +0530

    feat: list cmd for helm

---
+++ cmd/helm/list.go
@@ -0,0 +1,26 @@
+package helm
+
+import (
+	"fmt"
+	"github.com/clouddrove/smurf/internal/helm"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all Helm releases.",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		releases, err := helm.HelmList("default") // Assuming 'default' namespace or make it a flag
+		if err != nil {
+			return err
+		}
+		for _, release := range releases {
+			fmt.Printf("%v\n", release.Name)
+		}
+		return nil
+	},
+}
+
+func init() {
+	selmCmd.AddCommand(listCmd)
+}
