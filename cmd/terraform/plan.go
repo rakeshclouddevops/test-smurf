2af54bcffb3983c4bc589c25ef9dde48fcb52570
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-07 01:54:12 +0530

    refactor: added --var, --var-file flag to plan cmd

--- cmd/terraform/plan.go
+++ cmd/terraform/plan.go
@@ -5,15 +5,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var varNameValue string
+var varFile string
+
 var planCmd = &cobra.Command{
 	Use:   "plan",
 	Short: "Generate and show an execution plan for Terraform",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		return terraform.Plan()
+		return terraform.Plan(varNameValue, varFile)
 	},
 }
 
 func init() {
+	// Add flags for -var and -var-file
+	planCmd.Flags().StringVar(&varNameValue, "var", "", "Specify a variable in 'NAME=VALUE' format")
+	planCmd.Flags().StringVar(&varFile, "var-file", "", "Specify a file containing variables")
+
 	stfCmd.AddCommand(planCmd)
 }
