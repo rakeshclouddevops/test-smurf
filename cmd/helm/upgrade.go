bc3e4692dcd820a3e27fdaa75662ee3737f6b3a7
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-07 01:32:28 +0530

    refactor: added flags --set, --atomic, --namespace,, --create-namespace, -f , timeout, --debug

--- cmd/helm/upgrade.go
+++ cmd/helm/upgrade.go
@@ -3,6 +3,17 @@ package helm
 import (
 	"github.com/clouddrove/smurf/internal/helm"
 	"github.com/spf13/cobra"
+	"time"
+)
+
+var (
+	setValues       []string
+	valuesFiles     []string
+	namespace       string
+	createNamespace bool
+	atomic          bool
+	timeout         time.Duration
+	debug           bool
 )
 
 var upgradeCmd = &cobra.Command{
@@ -12,10 +23,17 @@ var upgradeCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		releaseName := args[0]
 		chartPath := args[1]
-		return helm.HelmUpgrade(releaseName, chartPath, "default") // Assuming 'default' namespace or make it a flag
+		return helm.HelmUpgrade(releaseName, chartPath, namespace, setValues, valuesFiles, createNamespace, atomic, timeout, debug)
 	},
 }
 
 func init() {
 	selmCmd.AddCommand(upgradeCmd)
+	upgradeCmd.Flags().StringSliceVar(&setValues, "set", []string{}, "Set values on the command line (can specify multiple or separate values with commas: key1=val1,key2=val2)")
+	upgradeCmd.Flags().StringSliceVarP(&valuesFiles, "values", "f", []string{}, "Specify values in a YAML file (can specify multiple)")
+	upgradeCmd.Flags().StringVarP(&namespace, "namespace", "n", "default", "Specify the namespace to install the release into")
+	upgradeCmd.Flags().BoolVar(&createNamespace, "create-namespace", false, "Create the namespace if it does not exist")
+	upgradeCmd.Flags().BoolVar(&atomic, "atomic", false, "If set, the installation process purges the chart on fail, the upgrade process rolls back changes, and the upgrade process waits for the resources to be ready")
+	upgradeCmd.Flags().DurationVar(&timeout, "timeout", 300*time.Second, "Time to wait for any individual Kubernetes operation (like Jobs for hooks)")
+	upgradeCmd.Flags().BoolVar(&debug, "debug", false, "Enable verbose output")
 }
