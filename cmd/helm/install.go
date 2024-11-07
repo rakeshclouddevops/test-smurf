eef0c2c1a58df02685b81171187d812f10fe47d0
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-11-07 18:49:52 +0530

    refactor: added --namespace flag during installation

--- cmd/helm/install.go
+++ cmd/helm/install.go
@@ -1,21 +1,28 @@
 package helm
 
 import (
-	"github.com/clouddrove/smurf/internal/helm"
-	"github.com/spf13/cobra"
+    "github.com/clouddrove/smurf/internal/helm"
+    "github.com/spf13/cobra"
 )
 
+var n string // This will hold the namespace value from the flag
+
 var installCmd = &cobra.Command{
-	Use:   "install [RELEASE] [CHART]",
-	Short: "Install a Helm chart into a Kubernetes cluster.",
-	Args:  cobra.ExactArgs(2),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		releaseName := args[0]
-		chartPath := args[1]
-		return helm.HelmInstall(releaseName, chartPath, "default") // Assuming 'default' namespace or make it a flag
-	},
+    Use:   "install [RELEASE] [CHART]",
+    Short: "Install a Helm chart into a Kubernetes cluster.",
+    Args:  cobra.ExactArgs(2),
+    RunE: func(cmd *cobra.Command, args []string) error {
+        releaseName := args[0]
+        chartPath := args[1]
+        if n == "" { // If no namespace is provided, use default
+            n = "default"
+        }
+        return helm.HelmInstall(releaseName, chartPath, n)
+    },
 }
 
 func init() {
-	selmCmd.AddCommand(installCmd)
+    // Add the namespace flag
+    installCmd.Flags().StringVarP(&n, "namespace", "n", "", "Specify the namespace to install the Helm chart into")
+    selmCmd.AddCommand(installCmd)
 }
